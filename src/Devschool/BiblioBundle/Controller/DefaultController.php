e704b22d64146d75c58180d701916cd38cb3c9de
Mathieu92000 <mathieugabillard@gmail.com>
2016-12-10 17:38:30 +0100

    Travail en cours

--- src/Devschool/BiblioBundle/Controller/DefaultController.php
+++ src/Devschool/BiblioBundle/Controller/DefaultController.php
@@ -2,29 +2,48 @@
 
 namespace Devschool\BiblioBundle\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller
 {
     /**
-     * @Route("/")
+     * @Route("/", name="page_accueil")
      */
     public function indexAction()
     {
         return $this->render('DevschoolBiblioBundle:Default:index.html.twig');
     }
-}
-/**
- * @Route("/cinema/{id}", requirements={"id": "\d+"})
- */
-public function showAction($id)
-{
-    $cinema = $this->getDoctrine()->getRepository('DevschoolBiblioBundle:cinema')->find($id);
 
-    return $this->render(
-        'DevschoolBiblioBundle:cinema:show.html.twig',
-        ['cinema' => $cinema]
-    );
-}
+    /**
+     * @Route("/cinema", name="cinema")
+     */
+    public function listAction()
+    {
+        $cinema = $this->getDoctrine()->getRepository('DevschoolBiblioBundle:Livre')->findAll();
 
+        $titre_des_films = 'Titres des films';
+
+        return $this->render(
+            'DevschoolBiblioBundle:Livre:list.html.twig',
+            ['livres' => $cinema, 'titre' => $titre_des_films]
+        );
+    }
+
+    /**
+     * @Route("/cinema/{id}", requirements={"id": "\d+"}, name="cinema")
+     */
+    public function showAction($id)
+    {
+        $cinema = $this->getDoctrine()->getRepository('DevschoolBiblioBundle:cinema')->find($id);
+
+        return $this->render(
+            'DevschoolBiblioBundle:cinema:show.html.twig',
+            ['cinema' => $cinema]
+        );
+    }
+
+
+}
